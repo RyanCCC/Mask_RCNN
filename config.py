24df54c03796d81bae5692e3c0c232c32e285c6c
chenjl <chenjl@augurit.com>
2022-04-27 16:37:14 +0800

    update config file

--- config.py
+++ config.py
@@ -10,11 +10,6 @@ def get_class(classes_path):
 
 class CustomerConfig(Config):
     NAME = "Customer"
-    GPU_COUNT = 1
-    # 应该通过设置IMAGES_PER_GPU来设置BATCH的大小，而不是下面的BATCH_SIZE
-    # BATCHS_SIZE自动设置为IMAGES_PER_GPU*GPU_COUNT
-    # 请各位注意哈！
-    IMAGES_PER_GPU = 1
     NUM_CLASSES = 1 + 3
     RPN_ANCHOR_SCALES = (16, 32, 64, 128, 256)
     IMAGE_MIN_DIM = 512
@@ -22,6 +17,7 @@ class CustomerConfig(Config):
     CLASSES = get_class('./data/shapes.names')
     TRAIN_DATASET = './train_data'
     PRETRAIN_MODEL = "model/mask_rcnn_coco.h5"
+    VALIDATION_STEPS = 50
     LEARNING_RATE = 1e-5
     EPOCH = 100
 
